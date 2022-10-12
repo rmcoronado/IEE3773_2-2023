81b32607e925145e42216263f8af0630dbed273c
rmcoronado <65876223+rmcoronado@users.noreply.github.com>
2022-10-12 10:23:58 -0300

    Delete Experiencia 3_ Separacion de agua y grasa directory

--- Experiencia 3_ Separacion de agua y grasa/src/itok.m
+++
@@ -1,9 +0,0 @@
-function [image] = itok(image, dimensions)
-
-if nargin < 2
-    image = ifftshift(fftn(ifftshift(image)));
-else
-    for d=1:numel(dimensions)
-        image = ifftshift(fft(ifftshift(image),[],dimensions(d)));
-    end
-end
--- Experiencia 3_ Separacion de agua y grasa/src/ktoi.m
+++
@@ -1,9 +0,0 @@
-function [image] = ktoi(image, dimensions)
-
-if nargin < 2
-    image = fftshift(ifftn(ifftshift(image)));
-else
-    for d=1:numel(dimensions)
-        image = fftshift(ifft(ifftshift(image),[],dimensions(d)));
-    end
-end
